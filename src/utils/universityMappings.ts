03c1fd57dd1d04079e080b274e630a2cef12279e
SamuelKatsaros <150530309+SamuelKatsaros@users.noreply.github.com>
2025-02-04 14:43:20 -0500

    new uni

--- src/utils/universityMappings.ts
+++ src/utils/universityMappings.ts
@@ -2,7 +2,8 @@ export const universityMap: Record<string, string> = {
   nyu: '123e4567-e89b-12d3-a456-426614174000',
   stonybrook: '3884b0da-b578-4e74-b921-e2d52dee1f71',
   harvard: 'ebd6ad52-dedd-4381-be9f-f16fa0686fe0',
-  newpaltz: '83705fa6-af38-4e4d-9a72-6115befd90a1'
+  newpaltz: '83705fa6-af38-4e4d-9a72-6115befd90a1',
+  sccc: '937be84f-c8df-40ea-9fc2-e4493d627206'
 };
 
 export const subdomainToUniversityId = (subdomain: string): string => {
